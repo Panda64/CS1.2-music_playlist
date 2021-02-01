afed919f13ca6453ebd452cd12569c7b5806d358
Panda64 <JaylenSchelb@yahoo.com>
2021-01-31 17:49:11 -0800

    Removed TODO's and added Docstrings

--- Playlist.py
+++ Playlist.py
@@ -2,21 +2,19 @@ from Song import Song
 
 class Playlist:
   def __init__(self):
-    self.__first_song = None
+    self.__first_song = None # <-- Head Pointer in disguise 
 
-
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
+    """Adds a song to the playlist"""
     new_song = Song(title)
     new_song.set_title(title)
     new_song.set_next_song(self.__first_song)
     self.__first_song = new_song
     
 
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
-
   def find_song(self, title):
+    """Finds a song within a playlist"""
     counter = 0
     current_song = self.__first_song
 
@@ -30,9 +28,8 @@ class Playlist:
     return counter
 
 
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
-
   def remove_song(self, title):
+    """Removes a song from a playlist"""
     
     current_song = self.__first_song
 
@@ -54,11 +51,9 @@ class Playlist:
     print("Song has been removed!")
     
 
-
-
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
-
   def length(self):
+    """Returns the length of the playlist"""
+
     counter = 0
     current_song = self.__first_song
 
@@ -69,20 +64,22 @@ class Playlist:
     return counter
 
 
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
-
   def print_songs(self):
-    counter = 1
+    """Prints the songs that are currently in the playlist"""
+
+    counter = 0
     current_song = self.__first_song
 
     while current_song != None:
+      counter += 1
       print(f"{counter}. {current_song.get_title()}")
       current_song = current_song.get_next_song()
-      counter += 1
+
+    if counter == 0:
+      print("There are no songs to display!")
+      
+      
+
+    
 
   
--- Song.py
+++ Song.py
@@ -5,31 +5,37 @@ class Song:
       self.__next_song = None
 
 
-  # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
+    """Getter method for the song title"""
+
     return self.__title
   
   
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
+    """Setter method for the song title"""
+
     self.__title = str(title).title()
 
 
-  # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
+    """Getter method for the next song object (node)"""
+    
     return self.__next_song
 
 
-  # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
+    """Setter method for the next song object (node)"""
+
     self.__next_song = next_title
 
 
-  # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
+    """Returns a human-friendly message of the current song title"""
+
     return f"Song Title: {self.get_title()}"
 
 
-  # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
+    """Returns a human-friendly message of the titles of the current song and the song next up"""
+
     return f"{self.get_title()} -> {self.get_next_song()}"
